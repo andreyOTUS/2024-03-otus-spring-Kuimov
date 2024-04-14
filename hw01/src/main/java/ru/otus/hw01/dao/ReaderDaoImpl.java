4c460fb5e131b542958a0d26219b0d96420871de
andre <andrey.kuimov2015@yandex.ru>
2024-04-15 00:13:28 +0300

    hw01: скорректировал Hw01 папку

--- hw01/src/main/java/ru/otus/hw01/dao/ReaderDaoImpl.java
+++ hw01/src/main/java/ru/otus/hw01/dao/ReaderDaoImpl.java
@@ -1,7 +1,5 @@
 package ru.otus.hw01.dao;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import ru.otus.hw01.config.TestFileNameProvider;
 
 import java.io.BufferedReader;
@@ -9,11 +7,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-@AllArgsConstructor
-@Data
-public class ReaderDaoImpl implements ReaderDao {
 
-    TestFileNameProvider fileNameProvider;
+public record ReaderDaoImpl(TestFileNameProvider fileNameProvider) implements ReaderDao {
 
     public InputStream inputStream() {
         return getClass().getClassLoader().getResourceAsStream(fileNameProvider.getTestFileName());
